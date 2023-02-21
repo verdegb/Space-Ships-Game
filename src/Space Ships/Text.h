b8f5492ab8c640f1037c86622e59d27df45045e2
Diego Gutiérrez <diegogb947@protonmail.com>
2023-02-21 15:18:47 +0100

    Space Ships: SDL2 Graphic Game

---
+++ src/Space Ships/Text.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Game.h"
+
+class Text
+{
+public:
+	Text(string content, float x, float y, Game* game);
+	void draw();
+	string content; //Text
+	int x;
+	int y;
+	int width;
+	int height;
+	Game* game;
+};
